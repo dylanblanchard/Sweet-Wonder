21275ea9d631e425ef41bc1b81944479f7bd8817
Dylan Blanchard <hi@dylan.pizza>
2019-08-21 17:42:42 -0400

    light this v2 life up

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var app = express();
-
-app.use(express.static(__dirname));
-
-var server = app.listen(8081, function(){
-    var port = server.address().port;
-    console.log("Server started at http://localhost:%s", port);
-});
